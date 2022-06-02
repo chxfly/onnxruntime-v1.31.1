6ce7ba04c53b0d6698190cf9adb59148cee8af62
Ye Wang <52801275+wangyems@users.noreply.github.com>
2022-06-02 15:45:46 -0700

    Update qordered_attention.py

--- onnxruntime/test/contrib_ops/qordered_python_test/qordered_attention.py
+++ onnxruntime/test/contrib_ops/qordered_python_test/qordered_attention.py
@@ -3,16 +3,13 @@ import numpy
 import os
 
 numpy.random.seed(0)
-scale = 0.007874015718698502
-weight_array = numpy.random.standard_normal(size = (768, 2304))
-bias_array = numpy.random.standard_normal(size = (2304))
+DATA_DIR = './qordered_attention'
 
 def create_qordered_attention_graph():
     from onnx import helper, numpy_helper, TensorProto
 
     nodes = [
         helper.make_node('QuantizeWithOrder', inputs=['input', 'scale_input'], outputs=['input_s8'], name='QuantizeWithOrder_0', domain='com.microsoft', order_input=1, order_output=1),
-        helper.make_node('QuantizeWithOrder', inputs=['weight_fp32', 'scale_weight'], outputs=['weight_col'], name='QuantizeWithOrder_1', domain='com.microsoft', order_input=1, order_output=0),
         helper.make_node(
             'QOrderedAttention',
             inputs=['input_s8', 'scale_input', 'weight_col', 'scale_weight', 'bias', 'scale_bias', 'scale_gemm', 'mask_index', 'scale_output'],
@@ -30,17 +27,17 @@ def create_qordered_attention_graph():
     ]
 
     initializers = [
-        numpy_helper.from_array(weight_array.astype('float32').reshape([768, 2304]), name='weight_fp32'),
-        numpy_helper.from_array(bias_array.astype('float32').reshape([2304]), name='bias'),
-        numpy_helper.from_array(numpy.array(scale, dtype='float32'), name='scale_input'),
-        numpy_helper.from_array(numpy.array(scale, dtype='float32'), name='scale_weight'),
-        numpy_helper.from_array(numpy.array(scale, dtype='float32'), name='scale_bias'),
-        numpy_helper.from_array(numpy.array(scale, dtype='float32'), name='scale_gemm'),
-        numpy_helper.from_array(numpy.array(scale, dtype='float32'), name='scale_output'),
+        numpy_helper.from_array(numpy.load(os.path.join(DATA_DIR, 'const59_1572_s8_COL.npy')).astype('int8').reshape([768, 2304]), name='weight_col'),
+        numpy_helper.from_array(numpy.load(os.path.join(DATA_DIR, 'const60_sentence_wise_att.layer.0.attention.self.merged_qkv_bias_fp32_0.0658777505159378.npy')).astype('float32').reshape([2304]), name='bias'),
+        numpy_helper.from_array(numpy.array(0.0013535089092329144, dtype='float32'), name='scale_weight'),
+        numpy_helper.from_array(numpy.array(0.0013535089092329144, dtype='float32'), name='scale_bias'),
+        numpy_helper.from_array(numpy.array(0.0658777505159378, dtype='float32'), name='scale_gemm'),
+        numpy_helper.from_array(numpy.array(0.020903365686535835, dtype='float32'), name='scale_output'),
     ]
 
     graph = helper.make_graph(nodes, "QOrderedAttention_Graph", [
         helper.make_tensor_value_info('input', TensorProto.FLOAT, [1, 32, 768]),
+        helper.make_tensor_value_info('scale_input', TensorProto.FLOAT, []),
         helper.make_tensor_value_info('mask_index', TensorProto.INT32, [1, 32]),
     ], [
         helper.make_tensor_value_info('output', TensorProto.FLOAT, [1, 32, 768]),
@@ -65,8 +62,8 @@ def create_attention_graph():
     ]
 
     initializers = [
-        numpy_helper.from_array(weight_array.astype('float32').reshape([768, 2304]), name='weight'),
-        numpy_helper.from_array(bias_array.astype('float32').reshape([2304]), name='bias'),
+        numpy_helper.from_array(numpy.load(os.path.join(DATA_DIR, 'const66_1572.npy')).astype('float32').reshape([768, 2304]), name='weight'),
+        numpy_helper.from_array(numpy.load(os.path.join(DATA_DIR, 'const3_sentence_wise_att.layer.0.attention.self.merged_qkv_bias.npy')).astype('float32').reshape([2304]), name='bias')
     ]
 
     graph = helper.make_graph(nodes, "Attention_Graph", [
@@ -87,11 +84,29 @@ sess_options = SessionOptions()
 ort_session_qattn = InferenceSession(qattn, sess_options, providers=['CUDAExecutionProvider'])
 ort_session_attn = InferenceSession(attn, sess_options, providers=['CUDAExecutionProvider'])
 
+input_data = numpy.random.standard_normal(size = (1, 32, 768)).astype('float32')
+mask_index_data = numpy.random.randint(1, 2, [1, 32], dtype=numpy.int32)
+input_scale_data = numpy.array(numpy.abs(input_data).max()/127).astype('float32')
+
 ort_inputs = {
-    'input' : numpy.random.standard_normal(size = (1, 32, 768)).astype('float32'),
-    'mask_index' : numpy.random.randint(1, 2, [1, 32], dtype=numpy.int32)
+    'input' : input_data,
+    'mask_index' : mask_index_data
 }
 
-ort_output_q = ort_session_qattn.run(None, ort_inputs)
 ort_output = ort_session_attn.run(None, ort_inputs)
-print(ort_output_q[0] - ort_output[0])
+
+ort_inputs['scale_input'] = input_scale_data
+
+ort_output_q = ort_session_qattn.run(None, ort_inputs)
+
+tol_l = 1e-5
+tol_r = 1e-1
+while (tol_r - tol_l > 1e-5):
+    tol = tol_l + (tol_r - tol_l)/2
+    if_close = numpy.allclose(ort_output_q[0], ort_output[0], rtol = tol, atol = tol)
+    if if_close is True:
+        tol_r = tol
+    else:
+        tol_l = tol
+
+print("atol/rtol threshold:", tol_r)
